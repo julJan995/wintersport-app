8fad55ec1d89dd721e39b7052be2777df4f14db8
juliavolk <julia.volk.dev@gmail.com>
2026-09-10 20:33:15 +0200

    feat: basic navbar implementation

---
+++ src/app/layout/navbar/navbar.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [MatIconModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {
+  isRegistered = false;
+}
